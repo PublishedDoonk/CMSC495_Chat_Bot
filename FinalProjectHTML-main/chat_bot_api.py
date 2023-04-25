12e8964ff016d1dc03198f799050dedd72ccb21d
PublishedDoonk <105333103+PublishedDoonk@users.noreply.github.com>
2023-04-24 22:07:16 -0400

    Update chat_bot_api.py
    Returns messages and answers as tuple to provide conversation context.

--- FinalProjectHTML-main/chat_bot_api.py
+++ FinalProjectHTML-main/chat_bot_api.py
@@ -1,6 +1,6 @@
 import openai
 
-def generateChatResponse(prompt,key):
+def generateChatResponse(prompt,key,messages):
     '''This fucntion will pull the user input and send it through the chatbot
     allowing the chatbot to generate a response. It will also receive the openai
     key allowing for the chatbot to make an API call to chatGPT.
@@ -8,15 +8,14 @@ def generateChatResponse(prompt,key):
     
     openai.api_key = key
     answer = "working"
-
     
-    messages = []
-    messages.append({"role": "system", "content": "You are a helpful assistant."})
+    if messages == []:
+        messages.append({"role": "system", "content": "You are a helpful assistant."})
 
-    question = {}
-    question['role'] = 'user'
-    question['content'] = prompt
-    messages.append(question)
+    messages.append({
+        'role': 'user',
+        'content': prompt,
+    })
 
     #using try/except blocks to catch potential errors, such as
     #incorrect api keys, disabled web access.
@@ -24,6 +23,7 @@ def generateChatResponse(prompt,key):
     try:
         response = openai.ChatCompletion.create(model="gpt-3.5-turbo",messages=messages)
         answer = response['choices'][0]['message']['content'].replace('\n', '<br>')
+        messages.append(response['choices'][0]['message'])
     except openai.error.AuthenticationError :
         answer = "You need to enter a valid key!"
     except KeyError:
@@ -32,4 +32,4 @@ def generateChatResponse(prompt,key):
         answer = "Unable to reach server"
 
     #returning the chatbot answer to the other chatroom program to be posted
-    return answer
+    return (answer, messages)
